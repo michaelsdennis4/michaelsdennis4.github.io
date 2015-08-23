f981f92e8f95641012cb9c47968521e8a25edb8d
Michael Dennis <michaelsdennis4@gmail.com>
2015-08-23 08:38:59 -0300

    working on re-design

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,21 @@
 console.log('main.js linked!');
+
+//EVENT LISTENERS
+
+$('.item#about').on('click', function(event) {
+	event.preventDefault();
+	$('.panel').html('<h1>ABOUT</h1>');
+});
+
+$('.item#contact').on('click', function(event) {
+
+});
+
+$('.item#skills').on('click', function(event) {
+
+});
+
+$('.item#projects').on('click', function(event) {
+
+});
+
