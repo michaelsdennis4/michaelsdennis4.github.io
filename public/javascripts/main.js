fc77a3b5c7c1727f89eb69ccf82b6611c0523421
Michael Dennis <michaelsdennis4@gmail.com>
2015-08-26 10:14:05 -0400

    added event listeners for pop up windows

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,20 +2,55 @@ console.log('main.js linked!');
 
 //EVENT LISTENERS
 
-$('.item#about').on('click', function(event) {
+$('.item#linkedin').on('click', function(event) {
 	event.preventDefault();
-	$('.panel').html('');
+	window.open(
+    "http://linkedin.com/in/michaelsdennis",
+    "Michael's Linked In",
+    "resizable,scrollbars,status"
+  );	
 });
 
-$('.item#contact').on('click', function(event) {
-
+$('.item#github').on('click', function(event) {
+	event.preventDefault();
+	window.open(
+    "http://github.com/michaelsdennis4",
+    "Michael's Github",
+    "resizable,scrollbars,status"
+  );
 });
 
-$('.item#skills').on('click', function(event) {
-
+$('a#project1').on('click', function(event) {
+	event.preventDefault();
+	window.open(
+    "https://maria-niforos-antiques.herokuapp.com/",
+    "Maria Niforos Antiques",
+    "resizable,scrollbars,status"
+  );
 });
 
-$('.item#projects').on('click', function(event) {
-
+$('a#project2').on('click', function(event) {
+	event.preventDefault();
+	window.open(
+    "http://sheltered-depths-2064.herokuapp.com/",
+    "Michael's Forum",
+    "resizable,scrollbars,status"
+  );
 });
+
+$('a#project3').on('click', function(event) {
+	event.preventDefault();
+	window.open(
+    "http://secure-sierra-4955.herokuapp.com/",
+    "Tic Tac Toe",
+    "resizable,scrollbars,status"
+  );
+});
+
+
+
+
+
+
+
 
